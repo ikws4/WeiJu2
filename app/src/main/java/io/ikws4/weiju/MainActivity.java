32495f9a809b760cf14d949f0d572c57a86ab03e
ikws4 <zhipingne@gmail.com>
2022-06-26 21:12:56 +0800

    feat(app): add luaj

--- app/src/main/java/io/ikws4/weiju/MainActivity.java
+++ app/src/main/java/io/ikws4/weiju/MainActivity.java
@@ -1,15 +1,38 @@
 package io.ikws4.weiju;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
+import io.ikws4.codeeditor.CodeEditor;
 
 public class MainActivity extends AppCompatActivity {
+    private CodeEditor mEditor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Globals globals = JsePlatform.standardGlobals();
+
+        mEditor = findViewById(R.id.code_editor);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener((menu) -> {
+            if (menu.getItemId() == R.id.menu_run) {
+                LuaValue chunk = globals.load(mEditor.getDocument().toString());
+                LuaValue result = chunk.call();
+                Toast.makeText(this, result + "", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        });
     }
 }
