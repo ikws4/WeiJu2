dfdff58c38a6a68e60d8f76e2dd403a3b36bff41
ikws4 <zhipingne@gmail.com>
2022-06-27 22:36:52 +0800

    feat(app): add xposed

---
+++ app/src/main/java/io/ikws4/weiju/xposed/XposedInit.java
@@ -0,0 +1,12 @@
+package io.ikws4.weiju.xposed;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedInit implements IXposedHookLoadPackage {
+
+  @Override
+  public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+
+  }
+}
