98faed48b5bf3aeaa98472387cdd99aae2f9de5a
ikws4 <zhipingne@gmail.com>
2022-08-02 15:27:39 +0800

    feat(xposed): add replace hook

--- app/src/main/java/io/ikws4/weiju/xposed/XposedLib.java
+++ app/src/main/java/io/ikws4/weiju/xposed/XposedLib.java
@@ -8,12 +8,10 @@ import org.luaj.vm2.lib.TwoArgFunction;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import org.luaj.vm2.lib.jse.CoerceLuaToJava;
 
-import java.util.Arrays;
-
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import io.ikws4.weiju.util.Logger;
 
 class XposedLib extends TwoArgFunction {
     @Override
@@ -43,6 +41,7 @@ class XposedLib extends TwoArgFunction {
             var clazz = table.get("class").checkjstring();
             var method = table.get("method");
             var params = table.get("params");
+            var replace = table.get("replace");
             var before = table.get("before");
             var after = table.get("after");
 
@@ -55,44 +54,59 @@ class XposedLib extends TwoArgFunction {
                 }
             }
 
-            _params[_params.length - 1] = new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) {
-                    call(before, param);
-                }
+            if (!replace.isnil()) {
+                _params[_params.length - 1] = new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) {
+                        LuaValue[] vargs = new LuaValue[1 + param.args.length];
+                        vargs[0] = CoerceJavaToLua.coerce(param.thisObject);
+                        for (int i = 0; i < param.args.length; i++) {
+                            vargs[i + 1] = CoerceJavaToLua.coerce(param.args[i]);
+                        }
 
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) {
-                    call(after, param);
-                }
-
-                private void call(LuaValue func, MethodHookParam param) {
-                    if (func.isnil()) return;
-
-                    LuaValue[] vargs = new LuaValue[1 + param.args.length];
-                    vargs[0] = CoerceJavaToLua.coerce(param.thisObject);
-                    for (int i = 0; i < param.args.length; i++) {
-                        vargs[i + 1] = CoerceJavaToLua.coerce(param.args[i]);
+                        Varargs ret = replace.invoke(vargs);
+                        if (ret.narg() == 0 || ret.arg1().isnil()) return null;
+                        return CoerceLuaToJava.coerce(ret.arg1(), param.getResult().getClass());
                     }
-                    Logger.d(Arrays.toString(vargs));
-
-                    Varargs ret = func.invoke(vargs);
-                    if (ret.narg() == 0) return;
-
-                    if (ret.arg1().isnil()) {
-                        param.setResult(null);
-                    } else {
-                        param.setResult(CoerceLuaToJava.coerce(ret.arg1(), param.getResult().getClass()));
+                };
+            } else {
+                _params[_params.length - 1] = new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) {
+                        call(before, param);
                     }
 
-                }
-            };
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        call(after, param);
+                    }
+
+                    private void call(LuaValue func, MethodHookParam param) {
+                        if (func.isnil()) return;
+
+                        LuaValue[] vargs = new LuaValue[1 + param.args.length];
+                        vargs[0] = CoerceJavaToLua.coerce(param.thisObject);
+                        for (int i = 0; i < param.args.length; i++) {
+                            vargs[i + 1] = CoerceJavaToLua.coerce(param.args[i]);
+                        }
+
+                        Varargs ret = func.invoke(vargs);
+                        if (ret.narg() == 0) return;
+
+                        if (ret.arg1().isnil()) {
+                            param.setResult(null);
+                        } else {
+                            param.setResult(CoerceLuaToJava.coerce(ret.arg1(), param.getResult().getClass()));
+                        }
+                    }
+                };
+            }
 
             XC_MethodHook.Unhook unhook;
             if (method.isnil()) {
                 unhook = XposedHelpers.findAndHookConstructor(clazz, XposedInit.classLoader, _params);
             } else {
-               unhook = XposedHelpers.findAndHookMethod(clazz, XposedInit.classLoader, method.checkjstring(), _params);
+                unhook = XposedHelpers.findAndHookMethod(clazz, XposedInit.classLoader, method.checkjstring(), _params);
             }
 
             return CoerceJavaToLua.coerce(unhook);
