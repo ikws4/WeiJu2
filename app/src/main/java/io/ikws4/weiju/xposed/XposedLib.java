2bdf334b9ccaf8e62f43f1dca77a40e6baaf3db2
ikws4 <zhipingne@gmail.com>
2022-08-03 10:05:25 +0800

    chore(xposed): remove 'log'

--- app/src/main/java/io/ikws4/weiju/xposed/XposedLib.java
+++ app/src/main/java/io/ikws4/weiju/xposed/XposedLib.java
@@ -10,28 +10,18 @@ import org.luaj.vm2.lib.jse.CoerceLuaToJava;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 
 class XposedLib extends TwoArgFunction {
     @Override
     public LuaValue call(LuaValue modname, LuaValue env) {
         LuaTable xposed = new LuaTable();
-        xposed.set("log", new log());
         xposed.set("hook", new hook());
         xposed.set("set_static_boolean_field", new set_static_boolean_field());
 
         env.set("xp", xposed);
         env.get("package").get("loaded").set("xp", xposed);
         return env;
-    }
-
-    static final class log extends OneArgFunction {
-        @Override
-        public LuaValue call(LuaValue arg) {
-            XposedBridge.log(arg.tojstring());
-            return NIL;
-        }
     }
 
     static final class hook extends OneArgFunction {
